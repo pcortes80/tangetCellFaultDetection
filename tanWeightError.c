37fb954ea8017172b1a5dc300a7cc464471a6495
Patricio Cortes <patricio.cortes@noirlab.edu>
2022-05-30 15:12:59 -0400

    Net moment forces calculations.

--- tanWeightError.c
+++ tanWeightError.c
@@ -12,19 +12,24 @@
 
 int main()
 {
-    double fa2 = 0.0, fa3 = 0.0, fa5 = 0.0, fa6 = 0.0; // tangential forces (N)
+    double fa1 = 0.0, fa2 = 0.0, fa3 = 0.0, fa4 = 0.0 , fa5 = 0.0, fa6 = 0.0; // tangential forces (N)
     double fw = 15140.0; // gravity force on mirror weight (N)
     double eA = 0.0; // elevation angle (°)
     double zd = 0.0; // zenith distance (°)
     double tanWeight; // tangential total wight (N)
     double loadBearing2, loadBearing3, loadBearing5, loadBearing6; // load bearing calculations (N) 
     double tanWeightError = 1000.0; // tangential witgh error (N)
+    double netMoments; // net moment of forces (N)
     
     // ask user enter values	
+    printf("Enter fa1 : ");
+    scanf("%lf",&fa1);
     printf("Enter fa2 : ");
     scanf("%lf",&fa2);
     printf("Enter fa3 : ");
     scanf("%lf",&fa3);
+    printf("Enter fa4 : ");
+    scanf("%lf",&fa4);
     printf("Enter fa5 : ");
     scanf("%lf",&fa5);
     printf("Enter fa6 : ");
@@ -39,13 +44,7 @@ int main()
     tanWeight = (-fa2 - fa3 + fa5 + fa6)*cos(30.0*(PI/180)) - fw*sin(zd*(PI/180));
       printf("Tangent Weight = %.2lf N\n", tanWeight);	
     double absTanWeight = abs(tanWeight); // absolute value of tanWeight
-
-    // compare limits
-    if(absTanWeight >= tanWeightError)
-      printf("¡Tangential Load Error!\n");
-    else
-      printf("No Tangential Load Error\n-");
-    
+ 
     // load bearing calculations
     loadBearing2 = fa2*cos(30.0*(PI/180)) + (fw*sin(zd*(PI/180)))/4;
       printf("Load Bearing 2 = %.3lf N\n", loadBearing2); 
@@ -55,6 +54,10 @@ int main()
       printf("Load Bearing 5 = %.3lf N\n", loadBearing5);
     loadBearing6 = fa6*cos(30.0*(PI/180)) - (fw*sin(zd*(PI/180))/4); 
       printf("Load Bearing 6 = %.3lf N\n", loadBearing6);
+
+    // net moment calculations
+    netMoments = fa1 + fa2 + fa3 + fa4 + fa5 + fa6; 
+      printf("Net Moments of Forces = %.3lf N\n", netMoments);
 
    return 0;
 }
